ea5d48a1ff0a6ca69b447d4c8fafcce0dd9dca2c
BENSALA <externe.amine.bensalah@allianz.fr>
2019-03-20 14:32:51 +0100

    first commit

---
+++ Sources/TaskQueue.h
@@ -0,0 +1,18 @@
+//
+//  TaskQueue.h
+//  TaskQueue
+//
+//  Created by BENSALA on 20/03/2019.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TaskQueue_iOS.
+FOUNDATION_EXPORT double TaskQueue_VersionNumber;
+
+//! Project version string for TaskQueue_iOS.
+FOUNDATION_EXPORT const unsigned char TaskQueue_VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TaskQueue/PublicHeader.h>
+
+
